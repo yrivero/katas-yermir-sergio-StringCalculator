c7b0d5008edeb1f219d5bb042f7415628ad155e4
yrivero <yermir@outlook.com>
2017-03-19 11:33:21 +0100

    New test When adding a single number the result must be that number Failing test

--- src/test/kotlin/com/hexad/katasyermirsergio/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/com/hexad/katasyermirsergio/stringcalculator/StringCalculatorTest.kt
@@ -19,6 +19,12 @@ class StringCalculatorTest {
         assertEquals("When adding empty string the result must be zero", 0, result)
     }
 
+    @Test
+    fun whenAddingASingleNumberMustReturnThatNumber() {
+        val calculator = StringCalculator()
+        val result = calculator.add("1")
 
+        assertEquals("When adding a single number the result must be that number", 1, result)
+    }
 }
 
