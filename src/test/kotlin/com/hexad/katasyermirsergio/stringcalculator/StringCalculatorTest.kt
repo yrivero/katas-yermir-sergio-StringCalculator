ca3174575f3f16677f86238707e03a1d6183d7f3
yrivero <yermir@outlook.com>
2017-03-19 11:28:37 +0100

    New test Adding an empty string must return 0 Failing test

--- src/test/kotlin/com/hexad/katasyermirsergio/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/com/hexad/katasyermirsergio/stringcalculator/StringCalculatorTest.kt
@@ -2,6 +2,7 @@ package com.hexad.katasyermirsergio.stringcalculator
 
 import org.junit.Test
 
+import org.junit.Assert.*
 
 class StringCalculatorTest {
 
@@ -9,6 +10,15 @@ class StringCalculatorTest {
     fun canCreateInstanceOfStringCalculator() {
         StringCalculator()
     }
+
+    @Test
+    fun whenAddingEmptyStringMustReturn0() {
+        val calculator = StringCalculator()
+        val result = calculator.add("")
+
+        assertEquals("When adding empty string the result must be zero", 0, result)
+    }
+
 
 }
 
