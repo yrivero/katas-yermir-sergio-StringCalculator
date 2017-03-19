908b148843150684aec7338a119aaf7e9250a6e3
yrivero <yermir@outlook.com>
2017-03-19 15:42:00 +0100

    New test When changing the delimiter the numbers are separated using it Passing test

--- src/main/kotlin/com/hexad/katasyermirsergio/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/com/hexad/katasyermirsergio/stringcalculator/StringCalculator.kt
@@ -7,18 +7,40 @@ class StringCalculator {
             return 0
         }
 
-        val numbers : List<Int> = parseNumbers(input)
+        var inputData : Array<String> = parseInputData(input)
+        val numberList = inputData[1]
+        val splittingRegex = inputData[0]
+
+        val numbers : List<Int> = parseNumbers(numberList, splittingRegex)
         val result = sumNumbers(numbers)
 
         return result
     }
 
-    fun parseNumbers(input : String) : List<Int>{
-        val numbers : List<String> = input.split(Regex(",|\n"))
+
+    fun parseNumbers(input : String, splittingRegex : String) : List<Int>{
+        val numbers : List<String> = input.split(Regex(splittingRegex))
         return numbers.map { x -> x.toInt()}
     }
 
     fun sumNumbers(input : List<Int>) : Int{
         return input.reduce { sum, number -> sum + number }
     }
+
+    private fun parseInputData(input: String): Array<String> {
+        if (input.startsWith("//")) {
+            val newLinePos = input.indexOf("\n")
+            val delimiter = input.substring(2, newLinePos)
+
+            val numberList = input.substring(newLinePos+1)
+
+            return arrayOf(delimiter, numberList)
+        }
+        else {
+            return arrayOf(",|\n", input)
+        }
+    }
+
+
+
 }
